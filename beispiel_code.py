6a2dca467a3d937341ae2377712acafd6860e40c
ulfsch126 <schlichtulf@gmail.com>
2025-04-22 08:57:37 +0000

    lesson done

---
+++ beispiel_code.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+## Variables
+#a = 10
+#b = 5
+#c = 3
+#
+## examples
+#summe = a + b
+#print("Summe:", summe)
+#
+#diff = a - b
+#print("Differenz", diff)
+
+# Aufgabe:
+# Führen Sie eine Addition, Subtraktion, Multiplikation und Division
+# mit zwei von Ihnen gewählten Variablen durch und geben Sie die Ergebnisse aus.
+
+alpha = 11
+beta = 4
+
+summe1 = alpha + beta
+print(alpha,"+",beta,"=",summe1)
+
+dif = alpha - beta
+print(alpha,"-",beta,"=",dif)
+
+multi = alpha + beta
+print(alpha,"*",beta,"=",multi)
+
+div = alpha / beta
+print(alpha,"/",beta,"=",div)
+divv = alpha // beta # es wird immer abgerundet, sprich die Zahl wird abgeschnitten
+print(alpha,"/",beta,"=",divv," (gerundet)")
